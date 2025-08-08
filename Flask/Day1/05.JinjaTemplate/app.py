9aab33d8a647a14dcbcce4833fda31f985082671
han-son-03 <rkdgksths11@naver.com>
2025-08-08 15:48:50 +0900

    강의와 다르게 해보기

--- Flask/Day1/05.JinjaTemplate/app.py
+++ Flask/Day1/05.JinjaTemplate/app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   data = {
-        'title': 'Flask Jinja Template',
+        'title': "Hanson's first flask",
         'user': 'Hanson',
         'is_admin': True,
-        'item_list': ['Item 1', 'Item 2', 'Item 3']
+        'item_list': ['역시', '백엔드가', '좋아']
     }
   
   
   # (1) redering할 html 파일명 입력
-  # (2) html로 넘ㅈ겨줄 데이터 입력
+  # (2) html로 넘겨줄 데이터 입력
   return render_template('index.html', data=data) 
 if __name__ == "__main__":
   app.run(debug = True)
